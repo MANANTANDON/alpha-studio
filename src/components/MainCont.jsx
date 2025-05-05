3eb449e7b9f01082404123299b0786df92db43ef
Manan Tandon <manantandon@Manans-MacBook-Air.local>
2025-05-05 09:03:40 +0530

    snowchild studios...

--- src/components/MainCont.jsx
+++ src/components/MainCont.jsx
@@ -1,4 +1,5 @@
 import { Box, Grid2, Typography } from "@mui/material";
+import Image from "next/image";
 import React from "react";
 
 export const MainCont = () => {
@@ -6,64 +7,20 @@ export const MainCont = () => {
     <>
       <Box
         sx={{
-          bgcolor: "#F8F3D9",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
           m: -1,
           height: "100vh",
-          position: "relative",
         }}
       >
-        <Box
-          sx={{
-            height: "5vh",
-            borderBottom: "2px solid black",
-            position: "absolute",
-            top: 0,
-            width: "100%",
-          }}
-        ></Box>
-        <Grid2 container sx={{ height: "100vh" }}>
-          <Grid2 size={0.5}></Grid2>
-          <Grid2
-            size={11}
-            sx={{
-              borderLeft: "2px solid black",
-              borderRight: "2px solid black",
-              pt: "20vh",
-              px: 3,
-            }}
-          >
-            <Typography
-              sx={{ fontSize: { xs: "20px", md: "50px" }, color: "#504B38" }}
-            >
-              <i>Welcome to,</i>
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: { xs: "40px", md: "80px" },
-                fontWeight: "bold",
-                color: "#504B38",
-              }}
-            >
-              Snowchild Studio
-            </Typography>
-            <Typography
-              sx={{ color: "#B9B28A" }}
-              fontSize={{ xs: "10px", md: "16px" }}
-            >
-              © Manan x Anshu
-            </Typography>
-          </Grid2>
-          <Grid2 size={0.5}></Grid2>
-        </Grid2>
-        <Box
-          sx={{
-            height: "5vh",
-            borderTop: "2px solid black",
-            position: "absolute",
-            bottom: 0,
-            width: "100%",
-          }}
-        ></Box>
+        <Image
+          src="/images/EnjoyTheShow.png"
+          alt="Enjoy The Show"
+          layout="intrinsic"
+          width={500}
+          height={500}
+        />
       </Box>
     </>
   );
